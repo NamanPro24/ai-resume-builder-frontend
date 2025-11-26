26de8999bbd45a9756c73ba7905b2f6d02f52b04
NamanPro24 <namanprogrammer24@gmail.com>
2025-11-26 20:07:28 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Layout from './Pages/Layout'
+import Dashboard from './Pages/Dashboard'
+import Resumebuilder from './Pages/Resumebuilder'
+import Preview from './Pages/Preview'
+import Login from './Pages/Login'
+import { useDispatch } from 'react-redux'
+import api from './configs/api'
+import { login, setLoading } from './app/features/authSlice'
+import {Toaster} from 'react-hot-toast'
+function App() {
+  const dispatch=useDispatch()
+
+  const getUserData=async()=>{
+    const token=localStorage.getItem('token')
+    try {
+      if(token){
+        const {data}=await api.get('/api/users/data',{headers:{Authorization:token}})
+        if(data.user){
+          dispatch(login({token,user:data.user}))
+        }
+        dispatch(setLoading(false))
+      }
+      else{
+         dispatch(setLoading(false))
+      }
+    } catch (error) {
+       dispatch(setLoading(false))
+       console.log(error.message)
+    }
+  }
+
+ useEffect(()=>{
+ getUserData()
+  },[])
+  return (
+    <>
+   <Toaster/>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      
+      {/* Nested routes inside Layout */}
+      <Route path='app' element={<Layout />}>
+        <Route index element={<Dashboard />} /> {/* default page */}
+        <Route path='builder/:resumeId' element={<Resumebuilder />} />
+      </Route>
+
+      <Route path='view/:resumeId' element={<Preview />} />
+    
+    </Routes>
+     </>
+  )
+}
+
+export default App
